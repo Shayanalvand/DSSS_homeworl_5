969d12e299786c077418919144738ad99ddd1b8d
shayanalvand <shayaan.alvandniya22@gmail.com>
2022-12-04 20:33:48 +0100

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='snowflake',
-    version='0.1.0',    
-    description='A example Python package',
-    url='https://github.com/Shayanalvand/DSSS_homeworl_5',
-    author='SHayan Alvandniya',
-    author_email='shayaan.alavandniya22@gmail.com',
-    license='Apache License',
-    packages=['snowflake'],
+    version='0.0.1',
+    author='shayan alvand',
+    author_email='shayaan.alvandiya22@gmail.com',
+    url='',
+    license='Apache 2.0',
+    packages=find_packages(),
+    description='Let you draw snowflakes'
 )
+

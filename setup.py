c8e64a29fe1e039bc9e708ce80a7dc3c510056d0
shayanalvand <shayaan.alvandniya22@gmail.com>
2022-12-04 20:17:41 +0100

    update

--- setup.py
+++ setup.py
@@ -9,16 +9,4 @@ setup(
     author_email='shayaan.alavandniya22@gmail.com',
     license='Apache License',
     packages=['snowflake'],
-    install_requires=['numpy', 'turtles'],
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
 )

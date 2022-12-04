8081b08e8dfb11ca635cbd11996faac829756279
shayanalvand <shayaan.alvandniya22@gmail.com>
2022-12-04 19:31:23 +0100

    preparing pip installable package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='snowflake',
+    version='0.1.0',    
+    description='A example Python package',
+    url='https://github.com/Shayanalvand/DSSS_homeworl_5',
+    author='SHayan Alvandniya',
+    author_email='shayaan.alavandniya22@gmail.com',
+    license='Apache License',
+    packages=['snowflake'],
+    install_requires=['turtle']
+)
